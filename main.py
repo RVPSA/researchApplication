63368084ac5f3a89706fc7c0d0c8317ba826a1e6
RVPSA <102511543+RVPSA@users.noreply.github.com>
2023-03-14 20:49:50 +0530

    create main canvas

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ import tkinter as tk
 from PIL import Image, ImageTk, ImageFont,ImageDraw
 from pocketsphinx import LiveSpeech
 import threading
+
+root = tk.Tk()
+root.title("Real-Time Sinhala Subtitle Generator")
+
+canvas = tk.Canvas(root, width=640, height=480)
+canvas.pack()
+
+
+root.mainloop()
